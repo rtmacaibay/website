111202fb7bc36f581722a9f115c6c1d4aab61bfb
Robert Macaibay <rtmacaibay@usfca.edu>
2020-07-07 11:32:50 -0700

    More testing

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
-var express = require('express');
-var app = express();
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const publicPath = path.join(__dirname + '/');
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(publicPath));
 
-app.listen(process.env.PORT || 8080);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+})
+
+app.listen(port, () => {
+    console.log(`Up on ${port}`);
+});
