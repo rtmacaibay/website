b9a514442afcbd4a00a604e36e1a664c6ef92dd7
rtmacaibay <robmacaibay@gmail.com>
2024-04-12 21:22:27 -0700

    add grecaptcha

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Header from '../components/Header';
 import Main from '../components/Main';
 import Footer from '../components/Footer';
 
+import grecaptcha from 'grecaptcha';
+
 const isBrowser = typeof window !== 'undefined';
 
 class IndexPage extends React.Component {
